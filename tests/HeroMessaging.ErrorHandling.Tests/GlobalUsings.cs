cfd03700c3469a4ad85647084cccc45865625662
Leishi L <ldsenow@gmail.com>
2025-11-13 15:29:15 +1100

    Remove InMemoryQueueStorageTests class and its associated unit tests

--- tests/HeroMessaging.ErrorHandling.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using Moq;
-global using HeroMessaging.Abstractions.Messages;
-global using HeroMessaging.Abstractions.Processing;
-global using HeroMessaging.Abstractions.ErrorHandling;
-global using HeroMessaging.ErrorHandling;
-global using Microsoft.Extensions.Logging;
-global using Microsoft.Extensions.Time.Testing;
--- tests/HeroMessaging.Policies.Tests/GlobalUsings.cs
+++
@@ -1,8 +0,0 @@
-global using Xunit;
-global using Moq;
-global using HeroMessaging.Abstractions.Messages;
-global using HeroMessaging.Abstractions.Processing;
-global using HeroMessaging.Abstractions.Policies;
-global using HeroMessaging.Policies;
-global using Microsoft.Extensions.Logging;
-global using Microsoft.Extensions.Time.Testing;
